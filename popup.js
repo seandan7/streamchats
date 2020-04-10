2876a7e84a7789c212c07a75dcdef578731fd554
unknown <sbd12b@gmail.com>
2020-04-10 14:41:44 -0400

    Add saved psuedofunctionality and basic UI

--- popup.js
+++ popup.js
@@ -5,5 +5,23 @@ saveButton.forEach((item) => {
     var messageToSave = event.currentTarget.parentNode.querySelector(
       ".message__text"
     ).innerHTML;
+    var messageToSaveSender = event.currentTarget.parentNode.querySelector(
+      ".message__name"
+    ).innerHTML;
+    var data = {
+      name: messageToSaveSender,
+      message: messageToSave,
+    };
+    fetch("http://localhost:4000/api/newMessage", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+    event.currentTarget.parentNode.querySelector(".message__text").classList +=
+      " saved";
   });
 });
